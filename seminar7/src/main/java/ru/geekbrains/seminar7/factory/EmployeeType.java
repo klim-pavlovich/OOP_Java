507dc1fa5bf47c7499e8d9f47b603cc10938b61c
klim-pavlovich <118849962+klim-pavlovich@users.noreply.github.com>
2023-09-29 23:47:28 +0300

    Delete seminar7 directory

--- seminar7/src/main/java/ru/geekbrains/seminar7/factory/EmployeeType.java
+++
@@ -1,6 +0,0 @@
-package ru.geekbrains.seminar7.factory;
-
-public enum EmployeeType {
-    Worker,
-    Freelancer
-}
--- seminar7/src/main/java/ru/geekbrains/seminar7/observer/hw/presenters/Publisher.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.seminar7.observer.hw.presenters;
-
-import ru.geekbrains.seminar7.observer.hw.models.Professions;
-
-public interface Publisher{
-    public void createVacancy(int salary, Professions specialization, int requiredExperienceYears);
-}
